a99e6bd6cd7eecff8a76ce042d56bdb14755abc1
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-11-13 21:41:20 +0100

    chore: done with react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
-
+import { Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
 function App() {
   return (
-    <div className="App">
-      <h1>shopping cart</h1>
-    </div>
+    <>
+      <div>
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Container>
+      </div>
+    </>
   );
 }
 
