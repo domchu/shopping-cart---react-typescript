649ce7554ccf64828fb5e527d2f5fcb4231278f5
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-11-14 17:01:08 +0100

    chore:format currency

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import formatCurrency from "./../utilities/formatCurrency";
 
 type storeItemProps = {
   id: number;
@@ -18,7 +19,7 @@ const StoreItem = ({ id, imgUrl, price, name }: storeItemProps) => {
       <Card.Body className="d-flex flex-column">
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-3">
           <span className="fs-0.7">{name}</span>
-          <span className="ms-2 text-muted">{price}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
       </Card.Body>
     </Card>
