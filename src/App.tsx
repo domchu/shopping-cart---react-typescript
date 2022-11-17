ab9cd53813865bbd6585c19ced5fa183df4d4699
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-11-17 18:28:24 +0100

    bug: fixed use shopping cart pathway

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Home from "./pages/Home";
 import Store from "./pages/Store";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
-import { ShoppingCartProvider } from "./context/shoppingCartContext";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 function App() {
   return (
     <ShoppingCartProvider>
--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,5 @@
 import { Card, Button } from "react-bootstrap";
-// import { useShoppingCart } from "../context/ShoppingCartContext";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import formatCurrency from "../utilities/formatCurrency";
 
 type storeItemProps = {
