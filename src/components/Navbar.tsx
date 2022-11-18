9f5839fc7dc84082b73db4cddbbb27944fded912
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-11-18 10:51:04 +0100

    chore:finished the cart maths

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Container, Navbar as NavbarBs, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { GiShoppingCart } from "react-icons/gi";
+import { useShoppingCart } from "./../context/ShoppingCartContext";
 
 const Navbar = () => {
+  const { openCart, cartQuantity } = useShoppingCart();
   return (
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
@@ -18,27 +20,30 @@ const Navbar = () => {
             About
           </Nav.Link>
         </Nav>
-        <button
-          style={{ width: "3rem", height: "3rem", position: "relative" }}
-          color="outline-primary"
-          className="rounded-circle"
-        >
-          <GiShoppingCart style={{ color: "blue", fontSize: "2rem" }} />
-          <div
-            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
-            style={{
-              width: "1.5rem",
-              height: "1.5rem",
-              position: "absolute",
-              color: "white",
-              top: "0",
-              right: "0",
-              transform: "translate(25%, 25%)",
-            }}
+        {cartQuantity > 0 && (
+          <button
+            onClick={openCart}
+            style={{ width: "3rem", height: "3rem", position: "relative" }}
+            color="outline-primary"
+            className="rounded-circle"
           >
-            5
-          </div>
-        </button>
+            <GiShoppingCart style={{ color: "blue", fontSize: "2rem" }} />
+            <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+              style={{
+                width: "1.5rem",
+                height: "1.5rem",
+                position: "absolute",
+                color: "white",
+                top: "0",
+                right: "0",
+                transform: "translate(25%, 25%)",
+              }}
+            >
+              {cartQuantity}
+            </div>
+          </button>
+        )}
       </Container>
     </NavbarBs>
   );
--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -9,10 +9,14 @@ type cartItem = {
   quantity: number;
 };
 type ShoppingCartContext = {
+  openCart: () => void;
+  closeCart: () => void;
   getItemQuantity: (id: number) => number;
   increaseItemQuantity: (id: number) => void;
   decreaseItemQuantity: (id: number) => void;
   removeFromCart: (id: number) => void;
+  cartQuantity: number;
+  cartItems: cartItem[];
 };
 const ShoppingCartContext = createContext({} as ShoppingCartContext);
 
@@ -21,7 +25,15 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider({ children }: shoppingCartProviderProps) {
+  const [isOpen, setIsOpen] = useState(false);
   const [cartItems, setCartItems] = useState<cartItem[]>([]);
+
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+  const openCart = () => setIsOpen(true);
+  const closeCart = () => setIsOpen(false);
 
   function getItemQuantity(id: number) {
     return cartItems.find((item) => item.id === id)?.quantity || 0;
@@ -33,7 +45,7 @@ export function ShoppingCartProvider({ children }: shoppingCartProviderProps) {
         return [...currItems, { id, quantity: 1 }];
       } else {
         return currItems.map((item) => {
-          if (item.id == id) {
+          if (item.id === id) {
             return { ...item, quantity: item.quantity + 1 };
           } else {
             return item;
@@ -44,11 +56,11 @@ export function ShoppingCartProvider({ children }: shoppingCartProviderProps) {
   }
   function decreaseItemQuantity(id: number) {
     setCartItems((currItems) => {
-      if (currItems.find((item) => item.id === id)?.quantity == 1) {
+      if (currItems.find((item) => item.id === id)?.quantity === 1) {
         return currItems.filter((items) => items.id !== id);
       } else {
         return currItems.map((item) => {
-          if (item.id == id) {
+          if (item.id === id) {
             return { ...item, quantity: item.quantity - 1 };
           } else {
             return item;
@@ -70,6 +82,10 @@ export function ShoppingCartProvider({ children }: shoppingCartProviderProps) {
         increaseItemQuantity,
         decreaseItemQuantity,
         removeFromCart,
+        cartQuantity,
+        cartItems,
+        openCart,
+        closeCart,
       }}
     >
       {children}
